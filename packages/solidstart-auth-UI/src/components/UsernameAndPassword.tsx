572a6c6bc87fd284a67c0acd301f73c1552f029e
Kayla <krspecie@gmail.com>
2024-09-12 01:38:38 +0100

    removing commented out code

--- packages/solidstart-auth-UI/src/components/UsernameAndPassword.tsx
+++ packages/solidstart-auth-UI/src/components/UsernameAndPassword.tsx
@@ -1,32 +1,6 @@
-// function twoSum(num: number){
-//     return num + 2;
-// }
-
-// module.exports = twoSum;
 
 import { JSX, Component, createSignal } from 'solid-js';
 import { useNavigate } from '@solidjs/router';
-
-// const UsernameAndPassword: Component = () => {
-//   const [loginStatus, setLoginStatus] = createSignal('');
-//   const [loginInput, setLoginInput] = createSignal({
-//     usernameInput: '',
-//     passwordInput: '',
-//   });
-
-//   return (
-//     <div class= "loginFormContainer">
-//       <input type="text" placeholder="Username" value={username()} onInput={(e) => setUsername(e.currentTarget.value)} />
-//       <input type="password" placeholder="Password" value={password()} onInput={(e) => setPassword(e.currentTarget.value)} />
-//       <button>Login</button>
-//     </div>
-//   );
-// };
-
-// export default UsernameAndPassword;
-
-// packages/package2/src/components/UsernameAndPassword.tsx
-
 
 const UsernameAndPassword = () => {
   const [username, setUsername] = createSignal('');
