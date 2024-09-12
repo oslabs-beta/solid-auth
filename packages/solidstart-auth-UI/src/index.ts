b6bb892992f8173b4391e202cda3e9804ba446e1
Kayla <krspecie@gmail.com>
2024-09-12 02:04:26 +0100

    remove more commented out code

--- packages/solidstart-auth-UI/src/index.ts
+++ packages/solidstart-auth-UI/src/index.ts
@@ -1,10 +1,3 @@
-// function isKayla(string: string){
-//     return string === 'Kayla'
-//     console.log("hello world")
-//     console.log("hello london!")
-// }
-// module.exports = isKayla;
-
 import UsernameAndPassword from "./components/UsernameAndPassword";
 
 export { UsernameAndPassword }
