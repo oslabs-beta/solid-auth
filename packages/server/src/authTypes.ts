6da0d5072cd9a615e77382b52234dca42c4291ec
Ryan Gilbert <ryanbgilbert@gmail.com>
2024-10-22 20:34:24 -0700

    Remove unnecessary param

--- packages/server/src/authTypes.ts
+++ packages/server/src/authTypes.ts
@@ -18,15 +18,13 @@ export interface AuthCallbacks {
   login: (
     username: string,
     password: string,
-    userLookupFunction: (username: string) => Promise<User | undefined>,
-    hashing: boolean
+    userLookupFunction: (username: string) => Promise<User | undefined>
   ) => Promise<User>;
   register: (
     username: string,
     password: string,
     userLookupFunction: (username: string) => Promise<User | undefined>,
-    userCreateFunction: (username: string, password: string) => Promise<User>,
-    hashing: boolean
+    userCreateFunction: (username: string, password: string) => Promise<User>
   ) => Promise<User>;
   logout: () => Promise<void>;
   validateUsername: (username: string) => string | undefined;
