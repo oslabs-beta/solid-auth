d063d3f2d71e38a632964fa981f1e069d563f364
Ryan Gilbert <ryanbgilbert@gmail.com>
2024-10-22 19:55:06 -0700

    Cleanup

--- test-folder/script.js
+++
@@ -1,4 +0,0 @@
-const isKaylaTrue = require('@solid-auth/solidstart-auth-ui')
-//var hi = require('@solid-auth/solidstart-auth-backend');
-//console.log(hi("midnight"));
-console.log(isKaylaTrue('Kayla'));
