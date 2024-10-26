53affc705dbc61e7abfa07cf9dae69d812c14e7c
Kayla <krspecie@gmail.com>
2024-10-26 22:31:10 +0100

    added isClient createSignal, onMount hook to check for browser environment.  Currently rendering component in demo app

--- packages/ui/src/components/login-UI.tsx
+++ packages/ui/src/components/login-UI.tsx
@@ -1,53 +1,63 @@
-import './login-UI.css';
-import { Component, createSignal } from 'solid-js';
-import solidLogo from '../../public/solid-auth-logo.png';
-import googleLogo from '../../public/google.png';
-import hideLogo from '../../public/hide.png';
+import "./login-UI.css";
+import { Component, createSignal, onMount, Show } from "solid-js";
+import solidLogo from "../../public/solid-auth-logo.png";
+import googleLogo from "../../public/google.png";
+import hideLogo from "../../public/hide.png";
 
 export const LoginForm: Component = () => {
   const [loginStatus, setLoginStatus] = createSignal(null);
   const [loginInput, setLoginInput] = createSignal({
-    usernameInput: '',
-    passwordInput: '',
+    usernameInput: "",
+    passwordInput: "",
   });
 
-  return (
-    <div class="loginFormContainer">
-      <form class="loginBox">
-        <div class="login-image">
-          <img src={solidLogo} alt="solidLogo" />
-        </div>
-        <h2 class="signInText">Sign In</h2>
+  const [isClient, setIsClient] = createSignal(false);
 
-        <input
-          class="form-control"
-          id="usernameInput"
-          type="email"
-          placeholder="Username"
-        />
-        <div class="password-container">
+  onMount(() => {
+    if (typeof window !== 'undefined'){
+      setIsClient(true)
+    }
+  });
+
+  return(
+    <Show when={isClient()}>
+      <div class="loginFormContainer">
+        <form class="loginBox">
+          <div class="login-image">
+            <img src={solidLogo} alt="solidLogo" />
+          </div>
+          <h2 class="signInText">Sign In</h2>
+
           <input
             class="form-control"
-            id="passwordInput"
-            type="password"
-            placeholder="Password"
+            id="usernameInput"
+            type="email"
+            placeholder="Username"
           />
-          <img src={hideLogo} alt="hide logo" class="hide-logo" />
-        </div>
-        <button class="log-in-button" type="submit">
-          Log In
-        </button>
-        <div class="lineBreakContainer"></div>
-        <button class="google-button" type="button">
-          <img src={googleLogo} class="google-logo" />
-          Sign in with Google
-        </button>
-        <div class="newUserLine">
-          <p>New to Solid Auth?</p>
+          <div class="password-container">
+            <input
+              class="form-control"
+              id="passwordInput"
+              type="password"
+              placeholder="Password"
+            />
+            <img src={hideLogo} alt="hide logo" class="hide-logo" />
+          </div>
+          <button class="log-in-button" type="submit">
+            Log In
+          </button>
+          <div class="lineBreakContainer"></div>
+          <button class="google-button" type="button">
+            <img src={googleLogo} class="google-logo" />
+            Sign in with Google
+          </button>
+          <div class="newUserLine">
+            <p>New to Solid Auth?</p>
 
-          <a href="#">Create New User</a>
-        </div>
-      </form>
-    </div>
-  );
-};
+            <a href="#">Create New User</a>
+          </div>
+        </form>
+      </div>
+      </Show>
+    );
+  };
